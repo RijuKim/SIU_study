4c9589ca8863b18655731a8a44dae59e50ac6f4a
RijuKim <rijoo1121@gmail.com>
2022-03-06 19:03:29 +0900

    Add: 자료형

--- Kotlin/Kotlin/01.변수/실습.kts
+++ Kotlin/Kotlin/01.변수/실습.kts
@@ -1,2 +1,14 @@
 //1. var 변수 선언하기
-var change_yes = 100
+var change_yes = 100 //선언과 동시에 초기화를 시켜줘야 한다
+println(change_yes)
+
+change_yes = 200
+println(change_yes)
+
+//2. val 변수 선언하기
+val CHAGE_NO = 100
+println(CHAGE_NO)
+//CHAGE_NO=200 //val cannot be ressigned
+
+val SHCOOL_NAME = "대한민국 학교"
+println(SCHOOL_NAME)
